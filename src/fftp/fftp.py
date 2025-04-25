60dad93fa195a4e75419ca6c23cb350c8ccf7cc1
Samir Benzammour <samir.benzammour@usd.de>
2025-04-25 15:10:18 +0200

    feat: logs credentials

--- src/fftp/fftp.py
+++ src/fftp/fftp.py
@@ -9,21 +9,22 @@ from pyftpdlib.authorizers import DummyAuthorizer
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler('tempftp.log')
+file_handler = logging.FileHandler("tempftp.log")
 file_handler.setLevel(logging.INFO)
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(message)s")
 file_handler.setFormatter(formatter)
 stream_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 
-MSG_LOGIN="Login successful"
-MSG_CLOSE="Goodbye"
+MSG_LOGIN = "Login successful"
+MSG_CLOSE = "Goodbye"
+
 
 # Class to log every action the user takes
 class CustomFTPHandler(FTPHandler):
@@ -31,7 +32,9 @@ class CustomFTPHandler(FTPHandler):
         logging.info(f"User '{username}' logged in successfully.")
 
     def on_login_failed(self, username, password):
-        logging.warning(f"Failed login attempt for user '{username}' with password '{password}'.")
+        logging.warning(
+            f"Failed login attempt for user '{username}' with password '{password}'."
+        )
 
     def on_file_received(self, file):
         logging.info(f"File received: {file}")
@@ -86,19 +89,22 @@ class AnyUserAuthorizer(DummyAuthorizer):
     """
     Authorization class that allows any combination of username/password.
     """
+
     def __init__(self, directory):
         DummyAuthorizer.__init__(self)
         self.directory = directory
         self.default_params = {
-            'pwd': '',
-            'home': self.directory,
-            'perm': 'elr',
-            'operms': {},
-            'msg_login': MSG_LOGIN,
-            'msg_quit': MSG_CLOSE,
+            "pwd": "",
+            "home": self.directory,
+            "perm": "elr",
+            "operms": {},
+            "msg_login": MSG_LOGIN,
+            "msg_quit": MSG_CLOSE,
         }
 
     def validate_authentication(self, username, password, handler):
+        logger.info(f"User '{username}' tried logging in with password: '{password}'")
+
         return True
 
     def get_home_dir(self, username):
@@ -108,10 +114,10 @@ class AnyUserAuthorizer(DummyAuthorizer):
         if username in self.user_table:
             return True
 
-        self.add_user(username, '', self.directory)
+        self.add_user(username, "", self.directory)
         return True
 
-    def has_perm(self, username, perm, path = None) -> bool:
+    def has_perm(self, username, perm, path=None) -> bool:
         if username not in self.user_table:
             # add user manually and not via add_user due to infinite recursion
             self.user_table[username] = self.default_params
@@ -123,6 +129,7 @@ class AnyUserAuthorizer(DummyAuthorizer):
 
     def get_msg_quit(self, username: str) -> str:
         return MSG_CLOSE
+
 
 class FastFTPServer:
     def __init__(self, directory, port):
@@ -147,15 +154,21 @@ class FastFTPServer:
             self.server.close_all()
         logging.info("FTP server shut down.")
 
+
 def signal_handler(sig, frame):
     logging.info("Received Ctrl+C, shutting down...")
     ftp_server.cleanup()
     exit(0)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Temporary FTP Server')
-    parser.add_argument('--dir', '-d', required=True, help='Directory to serve files from')
-    parser.add_argument('--port', '-p', type=int, default=21, help='Port to run the FTP server on')
+    parser = argparse.ArgumentParser(description="Temporary FTP Server")
+    parser.add_argument(
+        "--dir", "-d", required=True, help="Directory to serve files from"
+    )
+    parser.add_argument(
+        "--port", "-p", type=int, default=21, help="Port to run the FTP server on"
+    )
 
     args = parser.parse_args()
 
@@ -168,6 +181,6 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
     ftp_server.start()
 
+
 if __name__ == "__main__":
     main()
-
